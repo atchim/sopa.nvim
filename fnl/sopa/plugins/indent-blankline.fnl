9eda5fc751eff7bf995c7ab995cd6b9323879da5
atchim <atchim.gama.silva@gmail.com>
2022-04-27 20:43:43 -0300

    fix(plugins/ident-blankline.nvim): fix context start colors

--- fnl/sopa/plugins/indent-blankline.fnl
+++ fnl/sopa/plugins/indent-blankline.fnl
@@ -5,7 +5,7 @@
 (local groups
   { :IndentBlanklineChar {:fg (hex 2) :nocombine true}
     :IndentBlanklineContextChar {:fg (hex 5) :nocombine true}
-    :IndentBlanklineContextStart :IndentBlanklineContextChar
+    :IndentBlanklineContextStart {:sp (hex 5) :underline true}
     :IndentBlanklineSpaceCharBlankline :IndentBlanklineChar})
 
 (fn init []
--- lua/sopa/plugins/indent-blankline.lua
+++ lua/sopa/plugins/indent-blankline.lua
@@ -1,4 +1,4 @@
-local groups = {IndentBlanklineChar = {fg = "#121212", nocombine = true}, IndentBlanklineContextChar = {fg = "#383838", nocombine = true}, IndentBlanklineContextStart = "IndentBlanklineContextChar", IndentBlanklineSpaceCharBlankline = "IndentBlanklineChar"}
+local groups = {IndentBlanklineChar = {fg = "#121212", nocombine = true}, IndentBlanklineContextChar = {fg = "#383838", nocombine = true}, IndentBlanklineContextStart = {sp = "#383838", underline = true}, IndentBlanklineSpaceCharBlankline = "IndentBlanklineChar"}
 local function init()
   return (require("sopa")).hi_groups(groups)
 end
